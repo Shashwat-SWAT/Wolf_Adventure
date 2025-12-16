b6d48a9f88c864766caa13f8b0bdf886338d7d5c
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2025-12-16 16:32:22 -0600

    Class functions, and test
    I added a function that checks of the wolf dodged, and if it did then did it counter attack giving out two variables in return for dmg delt in counter and Truth value for if it dodged or not.

--- characters.py
+++ characters.py
@@ -2,17 +2,32 @@ import random
 
 class wolf:
     def __init__(self):
-        self._hp = 10
+        self._hp = 10 # wolf's hit points.
 
     def attack(self):
-        crit_chance = random.randint(1,10) # Probability of out of 10
+        crit_chance = random.randint(1,10) # Probability: out of 10
         atk_dmg = random.randint(3,5)
 
         if crit_chance >= 9: # 20% probability
             atk_dmg = atk_dmg + ((atk_dmg * 40)//100) # 40% crit dmg
-    
-    def dou
 
+        return atk_dmg
+    
+    def dodge_n_attack(self):
+        dodge_chances = random.choice([True,False]) # 50% probability
+        dodge_n_attack_chances = random.randint(1,10) # Probability: out of 10
+        dodge_attack_dmg = 0
+
+        if dodge_chances == False: # didn't dodge
+            dodge_attack_dmg = 0
+        
+        else: # dodged
+            if dodge_n_attack_chances >= 7: # 40% probability
+                dodge_attack_dmg = random.randint(1,3)
+            else:
+                dodge_attack_dmg = 0
+
+        return dodge_chances, dodge_attack_dmg
 
 class adventurer:
 
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from characters import wolf, adventurer
 from game import GAME
+import random
 
 def main():
-    
-    GAME.run()
+    a_wolf = wolf()
+    did_it_dodge, dodge_dmg = a_wolf.dodge_n_attack()
+    atk_dmg_wolf = a_wolf.attack()
+    print(did_it_dodge, dodge_dmg, atk_dmg_wolf)
+    #GAME.run()
 
 if __name__ == "__main__":
     main()
